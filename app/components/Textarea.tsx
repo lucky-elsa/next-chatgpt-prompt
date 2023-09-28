4be04f3499eb0cf7bc4cff438caffe4583f37ed7
beautylotus <beautylotus718@gmail.com>
2023-09-28 12:11:29 -0700

    update client component commit

---
+++ app/components/Textarea.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+function Textarea({}) {
+  return (
+    <textarea
+      
+      className="border-[1px] border-[#787878] rounded-[7px] h-[35rem] w-[20rem] p-[15px] text-[16px]"
+    ></textarea>
+  );
+}
+
+export default Textarea;
